61861f195745070092052d82fe13b35c1c579f6f
yuralala8 <yc1435@nyu.edu>
2017-09-11 09:18:15 -0400

    added search by address

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -2,17 +2,28 @@ require 'rest-client'
 class Api::V1::RestaurantsController < ApplicationController
 
 	def index
+		
+		place = params[:location]
+
+		geourl = "https://maps.googleapis.com/maps/api/geocode/json?address="+place+"&key=AIzaSyDCkp7F1EoltHsrpSOWr2VG2GhPGR-o7jg"
+		lonlat = JSON.parse(RestClient.get(geourl, headers={'Accpet': 'application/json'}))
+
+		lat = lonlat.dig('results')[0].dig('geometry').dig('location').dig('lat').to_s
+		lon = lonlat.dig('results')[0].dig('geometry').dig('location').dig('lng').to_s
+
 		term = params[:search]
-		url ="https://developers.zomato.com/api/v2.1/search?q="+term+"&lat=40.705253&lon=-74.014070"
+
+		url ="https://developers.zomato.com/api/v2.1/search?q="+term+"&lat="+lat+"&lon="+lon
 		restaurants = JSON.parse(RestClient.get(url, headers={'user-key': '46d92401d04f692193843afff4ed5aa4', 'Accept': 'application/json'}))
 		render json: restaurants
+
 	end
 
 
 	private
 
 	def restaurant_params
-		params.require(:data).permit(:name)
+		params.require(:data).permit(:search, :location)
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 	namespace :api do
     	namespace :v1 do
 			# resources :users
-			get '/restaurants/:search', to: 'restaurants#index'
+			get '/restaurants/:search/:location', to: 'restaurants#index'
 			# post '/login', to: 'auth#create'
 			# get '/me', to: 'users#me'
     	end
